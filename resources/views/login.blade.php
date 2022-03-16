262b47b61adfab962a0d0808416a5ec9089cffa0
amaliaanjani <amaliaanjani821@gmail.com>
2022-03-16 16:10:51 +0700

    commit kedua

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,7 +14,7 @@
 <head>
 	<title>Slide Navbar</title>
 	<link rel="stylesheet" type="text/css" href="Login style.css">
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet"> 
 </head>
 <body>
 	<div class="main">  	
@@ -23,7 +23,7 @@
 			<div class="signup">
 				<form>
 					<label for="chk" aria-hidden="true">Sign up</label>
-					<input type="text" name="txt" placeholder="User name" required="">
+					<input type="text" name="txt" placeholder="Username" required="">
 					<input type="email" name="email" placeholder="Email" required="">
 					<input type="password" name="pswd" placeholder="Password" required="">
 					<button>Sign up</button>
